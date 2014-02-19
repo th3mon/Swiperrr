c842070f3f850a9e6851a345b450025230b7376a
Przemysław Szelenberger <p.szelenberger@gmail.com>
2014-02-19 17:21:17 +0100

    ADD: CSS, images and demo

---
+++ demo/0/js/main.js
@@ -0,0 +1,8 @@
+(function (g, d, $, u){
+    (new Swiperrr({
+        numberOfLinks: 7,
+        wrapper: 'Swiperrr--wrapper',
+        list: $('.Swiperrr ul')
+    }));
+}(window, document, jQuery));
+    
